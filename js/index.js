f54208115a99ef09a906696b7bd975dad1bd2afe
Kevin Robb <kevin.robb@ou.edu>
2020-02-09 09:04:07 -0600

    added stories and updated mad lib word types

--- js/index.js
+++ js/index.js
@@ -1,6 +1,10 @@
 // Configuration
-madlibCategories = ['noun1', 'animal', 'mineral', 'noun2', 'noun3', 'noun4']
-story = "Welcome to <%noun1>! We hope you enjoy riding on our pristine <%animal>. We have many <%mineral>'s too!";
+madlibCategories = ['noun1', 'animal', 'adj', 'noun2plural', 'place', 'noun3']
+story1 = "I'm an NPC! \n Welcome to <%noun1>land! You will find that every citizen has their very own pet <%animal>, and the one assigned to you is especially <%adj>. Every person will find some sentient <%noun2plural> to join them in their quest to <%place>, as decreed by the oracle of <%noun3>. It seems like it's your lucky day, and your quest will begin shortly.";
+story2 = "I'm a Narrator! \n Weary traveler! It seems you have lost your way in the forest of <%noun2plural>. If it weren't for your <%adj> <%animal> who roped you into this trip, you wouldn't be in <%place> in the first place! That's alright! You have your wits, your broken <%noun3>, and your trusty <%noun1>, so this should turn out fine."
+story3 = "I'm an NPC! \n Howdy stranger! We don't take kindly to <%noun2plural> 'round here... This ain't no <%place> like you're used to. I suggest you mosey on over to the <%adj> shack and meet with the town leader. He's half <%animal>, so you best watch yourself. Once you have his <%noun1>, you'll have access to the wilds beyond the city, where you can continue your quest for the <%noun3>."
+stories = [story1, story2, story3]
+
 
 // Ignore these pls
 socket = null;
@@ -44,6 +48,9 @@ function continueFirst(event) {
 }
 
 function completeStory() {
+    // TODO select one of the stories based on dice results (random for now)
+    story = stories[Math.floor(Math.random() * 3)]
+
     madlibCategories.forEach(cat => {
         story = story.replace("<%" + cat + ">", madlibResults[cat])
     });
